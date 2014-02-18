5e1fb6ff7d31a2deada84c0c4b7a229ac4c48d05
promotelife <alex.qinyu@gmail.com>
2014-02-18 17:41:47 +0800

    add helper

--- helper/src/com/genius/helper/mssql.java
+++ helper/src/com/genius/helper/mssql.java
@@ -1,6 +1,5 @@
 package com.genius.helper;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
---
+++ helper/src/test.java
@@ -0,0 +1,21 @@
+import com.genius.helper.*;
+
+
+public class test {
+public static void main(String args[])
+{
+
+	/*
+	extractLog test_e = new extractLog();
+	test_e.tail(new File("d:/a.txt"));
+	
+	mysql test_mysql = new mysql();
+	test_mysql.setDatabase("pgenius");
+	test_mysql.getData("select * from STK_MKT limit 10;");
+	
+	*/
+	mssql test_mssql = new mssql();
+	test_mssql.setDatabase("cgenius");
+	test_mssql.getData("select top 100 * from stk_mkt;");
+	}
+}
